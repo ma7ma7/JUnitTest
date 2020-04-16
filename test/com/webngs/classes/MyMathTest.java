9e432deb3edf3d6bb2738fa0bc016030d7dcea7f
Amane Mahmoud <mahmoudamane5@gmail.com>
2020-04-16 19:14:13 +0100

    Use different annotations like @before

--- test/com/webngs/classes/MyMathTest.java
+++ test/com/webngs/classes/MyMathTest.java
@@ -2,11 +2,22 @@ package com.webngs.classes;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
 
 	MyMath myMath = new MyMath();
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Before Class");
+	}
+	
+	@Before
+	public void before() {
+		System.out.println("Before test 3 numbers");
+	}
 	
 	@Test
 	public void sum_3numbers() {
